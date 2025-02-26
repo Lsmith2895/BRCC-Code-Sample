0ce4901b8ac75f5bca3928c50d01d15e81cfbcc0
Logan Smith <lsmith2895@yahoo.com>
2025-02-26 16:56:43 -0500

    remove comments for previously completed todos (clean up)

--- document-nodes/todo.js
+++ document-nodes/todo.js
@@ -1,13 +1,11 @@
 import gql from 'graphql-tag';
 
-// TODO: implement a GET_TODOS query that will be used in the appolloClient
 export const GET_TODOS = gql`
 query GetTodos {
   todos
 }
 `;
 
-// TODO: implement a ADD_TODOS mutation that will be used in the appolloClient
 export const ADD_TODO = gql`
 mutation AddTodo($data: JSON!) {
   createTodo(data: $data) {
@@ -16,16 +14,14 @@ mutation AddTodo($data: JSON!) {
     completed
   }
 }
-`;;
+`;
 
-// TODO: implement a DELETE_TODOS mutation that will be used in the appolloClient
 export const DELETE_TODO = gql`
 mutation DeleteTodo($where: JSON!) {
   deleteTodo(where: $where)
 }
 `;
 
-// TODO: implement a UPDATE_TODOS mutation that will be used in the appolloClient
 export const UPDATE_TODO = gql`
 mutation UpdateTodo($values: JSON!, $options: JSON!) {
   updateTodo(values: $values, options: $options) {
