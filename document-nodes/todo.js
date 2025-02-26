36ffac902bd8e57362d9a5b8658995e899542dc6
Logan Smith <lsmith2895@yahoo.com>
2025-02-26 15:50:16 -0500

    create add delete and update todos (untested)

--- document-nodes/todo.js
+++ document-nodes/todo.js
@@ -8,13 +8,33 @@ query GetTodos {
 `;
 
 // TODO: implement a ADD_TODOS mutation that will be used in the appolloClient
-export const ADD_TODO = gql``;
+export const ADD_TODO = gql`
+mutation AddTodo($data: JSON!) {
+  createTodo(data: $data) {
+    id
+    title
+    completed
+  }
+}
+`;;
 
 // TODO: implement a DELETE_TODOS mutation that will be used in the appolloClient
-export const DELETE_TODO = gql``;
+export const DELETE_TODO = gql`
+mutation DeleteTodo($where: JSON!) {
+  deleteTodo(where: $where)
+}
+`;
 
 // TODO: implement a UPDATE_TODOS mutation that will be used in the appolloClient
-export const UPDATE_TODO = gql``;
+export const UPDATE_TODO = gql`
+mutation UpdateTodo($values: JSON!, $options: JSON!) {
+  updateTodo(values: $values, options: $options) {
+    id
+    title
+    completed
+  }
+}
+`;
 
 export default {
     GET_TODOS,
