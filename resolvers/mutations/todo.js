6eb4c98a0bf99d616fe4045a00e4553448a846c6
Logan Smith <lsmith2895@yahoo.com>
2025-02-27 14:51:28 -0500

    remove completed todo commetns

--- resolvers/mutations/todo.js
+++ resolvers/mutations/todo.js
@@ -2,7 +2,6 @@ import { v4 as uuidv4 } from 'uuid'
 import db from '../../models'
 
 const todo = {
-  // TODO: Implement the createTodo mutation
   async createTodo(parent, args, context, info) {
     
     try {
