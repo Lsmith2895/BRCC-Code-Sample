5927dc158f2abd1fca7f7751dab0ac271afcc2bf
Logan Smith <lsmith2895@yahoo.com>
2025-02-27 16:35:01 -0500

    up[date styling of todo list elements to match video

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,33 @@
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, Typography, CardActions, Button, Checkbox } from '@material-ui/core';
 // import {useMutation} from '@apollo/react-hooks';
  import todo, {DELETE_TODO, GET_TODOS, UPDATE_TODO} from '../document-nodes/todo';
 
 // NOTE: we typically use TypeScript in our codebase, but for this coding assessment we suggest using JSDoc instead.
 
-// TODO: implement styling
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  card: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: theme.spacing(2),
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    padding: theme.spacing(1),
+  },
+  cardContent: {
+    display: "flex",
+    alignItems: "center",
+    flexGrow: 1, 
+  },
+  checkbox: {
+    marginRight: theme.spacing(1),
+  },
+  cardActions: {
+    paddingRight: theme.spacing(2),
+  },
+}));
 
-// TODO: Update TodoList component to render list items.
 const TodoList = ({todos}) => {
   const classes = useStyles();
 
@@ -17,15 +37,24 @@ const TodoList = ({todos}) => {
 
   return (
     <div>
-      {todos.todos.map((todo =>  {
-      return (<div key={todo.id}>
-        <div>{todo.title}</div>
-        <div>{todo.completed.toString()}</div>
-      </div>
-      )
-    }))}
+      {todos.todos.map((todo) => (
+        <Card key={todo.id} className={classes.card}>
+          <CardContent className={classes.cardContent}>
+            <Checkbox
+              className={classes.checkbox}
+              checked={todo.completed}
+              onChange={() => onToggleComplete(todo.id, !todo.completed)}
+              color="primary"
+            />
+            <Typography className={classes.title}>{todo.title}</Typography>
+          </CardContent>
+          <CardActions className={classes.cardActions}>
+            <Button size="small" color="secondary">Delete</Button>
+          </CardActions>
+        </Card>
+      ))}
     </div>
-  );
+  );  
 };
 
 export default TodoList;
